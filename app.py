dfce4a2bfb6e4501749f1d8c3a6efe674ac43357
sanshyakapoor <66163544+sanshyakapoor@users.noreply.github.com>
2021-08-05 18:11:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,10 +22,6 @@ def welcome():
 def predict_price(final_features):
 	pred_price = model.predict(final_features)
 	return pred_price
-def string_to_datetime(string, current_format, to_format=None):
-	if to_format:
-		return datetime.strptime(datetime.strptime(string, current_format).strftime(to_format), to_format)
-	return datetime.strptime(string,current_format)
 
 def main():
 	st.title("Starbucks Stock Price Prediction")
@@ -43,8 +39,8 @@ def main():
 	# Getting the start day and next day from the dataset
 	start_day = stock_data.index[0]
 	last_day = stock_data.index[-1]
-	last_day1= string_to_datetime(last_day,'%d/%m/%Y')
-	next_day = last_day1 + datetime.timedelta(days = 1)
+	
+	next_day = last_day + datetime.timedelta(days = 1)
 
 	# Taking date input
 	input_date = st.date_input("Enter a Date: ", next_day)
